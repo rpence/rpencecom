8a1e94543a5433124f5ef6c3106a2edad091d035
Ronnie Pence <ronniepence@Ronnies-MacBook-Pro.local>
2020-07-02 00:12:43 -0400

    rm savior

--- components/VideoGallery.js
+++ components/VideoGallery.js
@@ -10,47 +10,31 @@ const useStyles = makeStyles((props) => {
             position: 'relative',
 
             '@media only screen and (max-width: 768px)': {
-                flexDirection: 'column',
-                flexGrow: 1,
-                flexShrink: 0,
+                flexBasis: '100%',
                 width: '100%',
-                flexBasis: '100%'
+                flexShrink: 0,
+                flexDirection: 'column'
+			},
+        },
+        videoContainer: {
+            padding: '10px',
+
+
+            '@media only screen and (max-width: 768px)': {
+                padding: '10px 0'
             },
 
-            '& div': {
-                padding: '10px',
-                width: '100%',
-                '@media only screen and (max-width: 768px)': {
-                    width: '100%',
-                    flexShrink: 0,
-                    padding: 0
-                },
-                '& iframe': {
-                    width: '100%',
-                    height: '100%',
-                    paddingBottom: 'calc(100% - 150px)',
-                    
-                    '@media only screen and (max-width: 768px)': {
-                        width: 'calc(100vw - 40px)',
-                        height: 'calc(100vw - 40px)',
-                        paddingBottom: 0,
-                        flexShrink: 0,
-                        marginBottom: '20px',
-
-                        '&:last-of-type': {
-                            marginBottom: '15px'
-                        }
-                    },
-                    
-                },
-                '&:first-of-type': {
-                    paddingLeft: 0
-                },
-                '&:last-of-type': {
-                    paddingRight: 0
-                }
-            }
+            '&:first-of-type': {
+                paddingLeft: 0
+            },
+            '&:last-of-type': {
+                paddingRight: 0
+            },
+        },
+        video: {
+            maxWidth: '100%',
         }
+
     }
 });
 
@@ -66,9 +50,17 @@ export default function VideoGallery(props) {
             <div className={classes.galleryBlock}>
                 {props.items.map((item) => {
                     return (
-                        <>
-                            <div dangerouslySetInnerHTML={createMarkup(`${item.video.html}`)} />
-                        </>
+                        <div className={classes.videoContainer}>
+                            <video
+                                muted
+                                autoPlay
+                                playsInline
+                                loop
+                                className={classes.video}
+                            >
+                                <source src={item.video_url} type="video/mp4" />
+                            </video>
+                        </div>
                     )
                 })}
             </div>
